a258afddac490ab7083a63162d5a8abb2ff4f25f
Amon <humfuzz@gmail.com>
2017-01-20 20:19:04 -0800

    holy canoli

---
+++ Assets/typer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class typer : MonoBehaviour {
+
+	public float freq;
+	public Text freq_text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Jump")) {
+			transform.position += new Vector3(0, 0.1f, 0);
+		}
+
+		Debug.Log(freq_text.text);
+
+		
+	}
+}
