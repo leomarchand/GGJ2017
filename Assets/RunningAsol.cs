ad0fedde7c2a98f8f413bdcec231bc6bc44678a3
Leo Marchand <leo@leomarchand.ca>
2017-01-21 20:04:51 -0800

    Added Asol

---
+++ Assets/RunningAsol.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunningAsol : MonoBehaviour {
+
+	private GameObject targetWave;
+	private Wave targetWaveScript;
+	private float xPosition;
+
+	// Use this for initialization
+	void Start () {
+		xPosition = 8.75f;
+		targetWave = GameObject.FindWithTag("wave");
+		targetWaveScript = targetWave.GetComponent<Wave>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var sprite = this.GetComponent<SpriteRenderer>().sprite;
+		Vector3 position = new Vector3(xPosition, targetWaveScript.getYForX(xPosition));
+		transform.position = position;
+	}
+}
