3a3c0adc79dcb54b5a323b5c2628e91ce7f017d5
Amon <humfuzz@gmail.com>
2017-01-21 22:27:20 -0800

    stuff

--- Assets/Scoreboard.cs
+++ Assets/Scoreboard.cs
@@ -4,8 +4,45 @@ using UnityEngine;
 
 public class Scoreboard : MonoBehaviour {
 
+	private struct highscore {
+		public string name;
+		public int score;
+
+		public highscore(string name, int score) {
+            this.name = name;
+			this.score = score;
+        }
+	}
+
+	private List<highscore> scoreList;
+
+
 	// Use this for initialization
 	void Start () {
+		// test debug
+
+		scoreList = new List<highscore>();
+
+		scoreList.Add(new highscore("MACHO", 69));
+		scoreList.Add(new highscore("MJ KIM", 68));
+
+		Debug.Log(scoreList[0].name);
+		Debug.Log(scoreList[1].score);
+
+		for (int i = 0; i < scoreList.Count; i++) {
+
+			PlayerPrefs.SetString(i.ToString() + "name", scoreList[0].name);
+			PlayerPrefs.SetInt(i.ToString() + "score", scoreList[0].score);
+
+		}
+
+		PlayerPrefs.Save(); 
+
+		// load scores from playerprefs
+
+
+		// update scores
+
 		
 	}
 	
